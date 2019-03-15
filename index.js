f2b1e49b252c6585dc948145d11b39c6a19ff22b
Martin <martin@zguy.com>
2019-03-15 14:24:26 +1100

    Changed the code to store data in a Google firestore DB

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloUser = (req, res) => {
-    let message = req.query.name || req.body.name || 'Stranger';
-    res.status(200).send('Hi there ' + message + '!');
+const Firestore = require('@google-cloud/firestore');
+// const PROJECTID = 'cloud-functions-firestore';
+const PROJECTID = 'functionstest-234600';
+const COLLECTION_NAME = 'cloud-functions-firestore';
+const firestore = new Firestore({
+  projectId: PROJECTID,
+  timestampsInSnapshots: true,
+});
+
+exports.martin_helloUser = (req, res) => {
+  if (req.method === 'DELETE') throw 'not yet built';
+  if (req.method === 'POST') {
+    // store/insert a new document
+    const data = (req.body) || {};
+    const ttl = Number.parseInt(data.ttl);
+    const ciphertext = (data.ciphertext || '').replace(/[^a-zA-Z0-9\-]*/g, '');
+    const created = new Date().getTime();
+    return firestore.collection(COLLECTION_NAME)
+      .add({ created, ttl, ciphertext })
+      .then(doc => {
+        return res.status(200).send(doc);
+      }).catch(err => {
+        console.error(err);
+        return res.status(404).send({ error: 'unable to store', err });
+      });
+  }
+  // read/retrieve an existing document by id
+  if (!(req.query && req.query.id)) {
+    return res.status(404).send({ error: 'No Id' });
+  }
+  const id = req.query.id.replace(/[^a-zA-Z0-9]/g, '').trim();
+  if (!(id && id.length)) {
+    return res.status(404).send({ error: 'Empty Id' });
+  }
+  return firestore.collection(COLLECTION_NAME)
+    .doc(id)
+    .get()
+    .then(doc => {
+      if (!(doc && doc.exists)) {
+        return res.status(404).send({ error: 'Unable to find the document' });
+      }
+      const data = doc.data();
+      return res.status(200).send(data);
+    }).catch(err => {
+      console.error(err);
+      return res.status(404).send({ error: 'Unable to retrieve the document' });
+    });
 };
-  
